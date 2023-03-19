f2fb4d6224a8060093e2443cba10e8b92d95e8bd
David Ano-Trudeau <89054564+Gretron@users.noreply.github.com>
2023-03-19 01:15:50 -0400

    frag(backend): add mqtt light switch base

--- src/backend/scripts/monitor.js
+++ src/backend/scripts/monitor.js
@@ -1,24 +1,54 @@
 const jwt = require("jsonwebtoken");
 const mqtt = require("mqtt");
+const raspi = require("raspi");
+// const gpio = require("raspi-gpio");
 
 /**
  * Start MQTT Client Monitor
  */
 const monitor = function () {
-  // TODO: Monitor Function Connecting to MQTT Broker
-  const adminToken = jwt.sign({ user_id: 1 }, process.env.SECRET);
+  raspi.init(() => {
+    // TODO: Monitor Function Connecting to MQTT Broker
+    const adminToken = jwt.sign({ user_id: 1 }, process.env.SECRET);
 
-  const options = {
-    host: process.env.MQTT_HOST,
-    port: process.env.MQTT_PORT,
-    username: adminToken,
-    password: "any",
-  };
+    const options = {
+      host: process.env.MQTT_HOST,
+      port: process.env.MQTT_PORT,
+      username: adminToken,
+      password: "any",
+    };
 
-  const client = mqtt.connect(options);
+    const client = mqtt.connect(options);
 
-  client.on("connect", () => {
-    console.log("Connected to MQTT Broker");
+    client.on("connect", () => {
+      console.log("Connected to MQTT Broker");
+
+      client.subscribe("safehouse/light");
+
+      client.on("message", (topic, message) => {
+        switch (topic) {
+          case "safehouse/light":
+            const output = new gpio.DigitalOutput(process.env.LIGHT_PIN);
+            const state = parseInt(message);
+
+            if (state === 1) {
+              output.write(gpio.HIGH);
+            } else if (state === 0) {
+              output.write(gpio.LOW);
+            } else {
+              console.error("Invalid Light State");
+            }
+
+            break;
+          default:
+            break;
+        }
+      });
+    });
+
+    setInterval(() => {
+      // console.log("Test");
+    }, 1000);
   });
 };
 
